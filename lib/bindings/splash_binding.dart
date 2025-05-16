3931ab929c6515466fcf3beae7ec0e70a6819ec0
Yorn Chanpenh <yornchanpenh666@gmail.com>
2025-05-16 15:20:32 +0700

    update git ignore

--- lib/bindings/splash_binding.dart
+++ lib/bindings/splash_binding.dart
@@ -1,7 +1,8 @@
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/get_instance.dart';
-import 'package:yeeahnis_movie/controllers/service_controller.dart';
-import 'package:yeeahnis_movie/controllers/splash_controller.dart';
+import 'package:get/get.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
+
+import '../controllers/service_controller.dart';
+import '../controllers/splash_controller.dart';
 
 class SplashBinding extends Bindings {
   @override
